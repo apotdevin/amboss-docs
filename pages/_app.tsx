53eb81a5238f6b044f852721afad45d16fcb7c90
apotdevin <potdevin.anthony@gmail.com>
2021-04-03 20:23:00 +0200

    chore: add title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import styled, { ThemeProvider } from 'styled-components';
 import { GlobalStyles } from '../src/styles/GlobalStyle';
 import { AppProps } from 'next/dist/next-server/lib/router/router';
+import Head from 'next/head';
 
 const Content: React.FC = ({ children }) => {
   return (
@@ -14,6 +15,11 @@ const Content: React.FC = ({ children }) => {
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Content>
+      <Head>
+        <title>
+          Amboss - Professional Node Manager for your Lightning Network Node
+        </title>
+      </Head>
       <Component {...pageProps} />
     </Content>
   );
