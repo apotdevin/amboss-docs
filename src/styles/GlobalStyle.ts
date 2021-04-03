98637aea20d2ec13622b50d1e480d9d9306a06a5
apotdevin <potdevin.anthony@gmail.com>
2021-04-03 20:41:08 +0200

    chore: add assets

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -1,4 +1,5 @@
 import { createGlobalStyle } from 'styled-components';
+import { FontStyles } from './Fonts';
 
 export const GlobalStyles = createGlobalStyle`
     html, body {
@@ -11,8 +12,11 @@ export const GlobalStyles = createGlobalStyle`
     body {
         background: white;
         color: black;
+        font-family: 'Inter', sans-serif;
         text-rendering: optimizeLegibility;
         -webkit-font-smoothing: antialiased;
         -moz-osx-font-smoothing: grayscale;
     }
+
+    ${FontStyles}
 `;
