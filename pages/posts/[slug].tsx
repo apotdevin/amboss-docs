e07e8ee3806736e48c55ce57e164be3e77f49d68
apotdevin <potdevin.anthony@gmail.com>
2021-04-03 20:18:43 +0200

    chore: small cleanup

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -1,15 +1,16 @@
-import fs from "fs";
-import matter from "gray-matter";
-import hydrate from "next-mdx-remote/hydrate";
-import renderToString from "next-mdx-remote/render-to-string";
-import { MdxRemote } from "next-mdx-remote/types";
-import dynamic from "next/dynamic";
-import Head from "next/head";
-import Link from "next/link";
-import path from "path";
-import CustomLink from "../../components/CustomLink";
-import Layout from "../../components/Layout";
-import { postFilePaths, POSTS_PATH } from "../../utils/mdxUtils";
+import fs from 'fs';
+import matter from 'gray-matter';
+import { GetStaticPaths, GetStaticProps } from 'next';
+import hydrate from 'next-mdx-remote/hydrate';
+import renderToString from 'next-mdx-remote/render-to-string';
+import { MdxRemote } from 'next-mdx-remote/types';
+import dynamic from 'next/dynamic';
+import Head from 'next/head';
+import Link from 'next/link';
+import path from 'path';
+import CustomLink from '../../components/CustomLink';
+import Layout from '../../components/Layout';
+import { postFilePaths, POSTS_PATH } from '../../utils/mdxUtils';
 
 // Custom components/renderers to pass to MDX.
 // Since the MDX files aren't loaded by webpack, they have no knowledge of how
@@ -20,11 +21,11 @@ const components = {
   // It also works with dynamically-imported components, which is especially
   // useful for conditionally loading components for certain routes.
   // See the notes in README.md for more details.
-  TestComponent: dynamic(() => import("../../components/TestComponent")),
+  TestComponent: dynamic(() => import('../../components/TestComponent')),
   Head,
 };
 
-export default function PostPage({ source, frontMatter }) {
+const PostPage = ({ source, frontMatter }: any) => {
   const content = hydrate(source, { components });
   return (
     <Layout>
@@ -57,10 +58,12 @@ export default function PostPage({ source, frontMatter }) {
       `}</style>
     </Layout>
   );
-}
+};
 
-export const getStaticProps = async ({ params }) => {
-  const postFilePath = path.join(POSTS_PATH, `${params.slug}.mdx`);
+export default PostPage;
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const postFilePath = path.join(POSTS_PATH, `${params?.slug}.mdx`);
   const source = fs.readFileSync(postFilePath);
 
   const { content, data } = matter(source);
@@ -83,12 +86,12 @@ export const getStaticProps = async ({ params }) => {
   };
 };
 
-export const getStaticPaths = async () => {
+export const getStaticPaths: GetStaticPaths = async () => {
   const paths = postFilePaths
     // Remove file extensions for page paths
-    .map((path) => path.replace(/\.mdx?$/, ""))
+    .map(path => path.replace(/\.mdx?$/, ''))
     // Map the path into the static paths object required by Next.js
-    .map((slug) => ({ params: { slug } }));
+    .map(slug => ({ params: { slug } }));
 
   return {
     paths,
